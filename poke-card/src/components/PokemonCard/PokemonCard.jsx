e9878b4b887710037bf4c9edee55938545f2b2ce
patricionarbona <patricioinarbona@gmail.com>
2024-04-29 08:33:41 +0200

    añadido svg spin loading

--- poke-card/src/components/PokemonCard/PokemonCard.jsx
+++ poke-card/src/components/PokemonCard/PokemonCard.jsx
@@ -3,8 +3,13 @@ import { useState, useEffect } from 'react'
 function PokemonCard() {
 
     const [ id, setId ] = useState(
-        Math.floor(Math.random() * (151 - 1 + 1)) + 1
+        // Math.floor(Math.random() * (1025 - 1 + 1)) + 1
+        // Math.floor(Math.random() * (151 - 1 + 1)) + 1
+        25
     );
+
+    const { pokemonData, setPokemon } = useState(null)
+    const fotin = pokemonData?.sprites.other.dream_world.front_default
 
     const [ foto , setFoto] = useState("rutaFoto")
 
@@ -33,7 +38,10 @@ function PokemonCard() {
         })
         .then(res => {
 
+            console.log("respuesta 2: ",res)
+            // setPokemon(res)
             let rutaFoto = res.sprites.other.dream_world.front_default;
+            // let rutaFoto = res.sprites.other.showdown.front_default;
             let nombreFormateado = res.name.charAt(0).toUpperCase() + res.name.slice(1);
             console.log(rutaFoto);
             setFoto(rutaFoto);
@@ -51,6 +59,7 @@ function PokemonCard() {
     }, [id])
 
     function handleClickAleatorio() {
+        // let random = Math.floor(Math.random() * (1025 - 1 + 1)) + 1;
         let random = Math.floor(Math.random() * (151 - 1 + 1)) + 1;
         setId( random);
     }
@@ -64,7 +73,7 @@ function PokemonCard() {
             <img src={foto} className="w-full h-full"/>
           </div>
           <div className='flex flex-col items-center'>
-            <p className='font-bold text-lg py-2'>{name}<span className='text-gray-500 font-light'>{vida}</span></p>
+            <p className='font-bold text-lg py-2'>{name} <span className='text-gray-500 font-light'>{vida}hp</span></p>
             <p className='text-gray-500 font-light pb-2'>{exp} exp</p>
           </div>
         </div>
@@ -74,7 +83,14 @@ function PokemonCard() {
               <p className="flex flex-col items-center font-extralight text-xs"><span className='font-bold'>{ataqueEspecial}K</span>Ataque Especial</p>
               <p className="flex flex-col items-center font-extralight text-xs"><span className='font-bold'>{defensa}K</span>Defensa</p>
           </div>
-          <button onClick={handleClickAleatorio} className="self-center bg-blue-500 text-white font-bold py-1 px-4 rounded hover:bg-blue-700">Generar nuevo Pokemon</button>
+          <button onClick={handleClickAleatorio} className=" flex justify-between items-center w-48 self-center bg-blue-500 text-white font-bold py-1 px-4 rounded hover:bg-blue-700">
+          <svg aria-hidden="true" className="w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+          </svg>
+            Generar Pokemon
+          </button>
+  
         </div>
       </div>
       </>
